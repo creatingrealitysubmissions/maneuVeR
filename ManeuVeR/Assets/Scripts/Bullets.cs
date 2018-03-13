f4670545792c872498ebedf7c22fb6caef5ecda2
Joshua Patrick <jpatrick@csumb.edu>
2018-03-13 16:29:14 -0700

    Added bullets, textures, and new lever model

---
+++ ManeuVeR/Assets/Scripts/Bullets.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+
+    public Rigidbody projectile;
+    public Transform barrel;
+
+    public float nextfire = 0.0f;
+    public float fireSpeed = .33f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1") && Time.time > nextfire)
+        {
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectile, barrel.position, barrel.rotation) as Rigidbody;
+            projectileInstance.AddForce(barrel.forward * 5000);
+
+            nextfire = Time.time + fireSpeed;
+        }
+	}
+}
