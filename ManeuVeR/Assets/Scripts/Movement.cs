fb733a2c0f9faa0df1ff2a576f80514f9929e7a3
Joshua Patrick <jpatrick@csumb.edu>
2018-03-13 23:24:51 -0700

    Finished handle script, started movement script

---
+++ ManeuVeR/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody leftThrust;
+
+	public Rigidbody righThrust;
+
+	public Rigidbody Player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	//if the thrusters are in idle position 
+		//if(leftThrust.transform.position.z >=- && righThrust.transform.position.z == 0)
+		{
+		//add no force
+
+		}
+			
+	}
+}
--- ManeuVeR/Assets/Scripts/cockpitMovement.cs
+++ ManeuVeR/Assets/Scripts/cockpitMovement.cs
@@ -10,17 +10,23 @@ public class cockpitMovement : MonoBehaviour {
 	private Rigidbody rbxrotation;
 	private Rigidbody rbxposition;
 
-	private Vector3 originalPosition;
+	private float speed = 5.0f;
 
+	private Vector3 originalPosition;
+	private Vector3 handlePosition;
 	private Vector3 tempPosition; 
+
+	public Rigidbody originalHandlePosition;
 
 	// Use this for initialization
 	void Start () {
-	//	rbxrotation = shifter.GetComponent<Rigidbody>();
+		rbxrotation = shifter.GetComponent<Rigidbody>();
 		rbxposition = this.GetComponent<Rigidbody>();
 
 		originalPosition = rbxposition.transform.localPosition;
 		tempPosition = originalPosition;
+
+		handlePosition = originalHandlePosition.transform.localPosition;
 
 	}
 
@@ -31,26 +37,47 @@ public class cockpitMovement : MonoBehaviour {
 		{
 			rbxposition.isKinematic = false;
 			rbxposition.useGravity = true;
+			if(rbxposition.transform.localPosition.z >= 0.22 || rbxposition.transform.localPosition.z <= -0.22)
+			 {	
+			 	//alex code
+			 	if(rbxposition.transform.localPosition.z >= .22) {
+					handlePosition.z = .22f;
+					rbxrotation.transform.localPosition = Vector3.Lerp(rbxrotation.transform.localPosition,handlePosition,speed * Time.deltaTime);
+			 		}
+			 	else{
+
+					handlePosition.z = -.22f;
+					rbxrotation.transform.localPosition = Vector3.Lerp(rbxrotation.transform.localPosition,handlePosition,speed * Time.deltaTime);
+			 	}
+		 	}
+		 	else {
+
+		 		handlePosition.z = rbxposition.transform.localPosition.z; 
+		 		//handlePosition = new Vector3(0f,.15f,rbxposition.transform.localPosition.z);
+		 		rbxrotation.transform.localPosition = Vector3.Lerp(rbxrotation.transform.localPosition,handlePosition,speed * Time.deltaTime);
+		 	}
+
 		}
 
 		if(!grabed)
 		 {
 			rbxposition.isKinematic = true;
 			rbxposition.useGravity = false;
-			if(!(rbxposition.transform.localPosition.x <= .18 && rbxposition.transform.localPosition.x >= -.18)){
+			if(!(rbxposition.transform.localPosition.z <= .22 && rbxposition.transform.localPosition.z >= -.22)){
 				rbxposition.transform.localPosition = tempPosition;
 			}
 			rbxposition.transform.localPosition = Vector3.Lerp(rbxposition.transform.localPosition, originalPosition, 0.5f * Time.deltaTime);
+			rbxrotation.transform.localPosition = new Vector3 (rbxrotation.transform.localPosition.x, rbxrotation.transform.localPosition.y,rbxposition.transform.localPosition.z);
 		 }
 
-		if(rbxposition.transform.localPosition.x >= 0.18 || rbxposition.transform.localPosition.x <= -0.18)
+		if(rbxposition.transform.localPosition.z >= 0.22 || rbxposition.transform.localPosition.z <= -0.22)
 		 {	
 		 	//alex code
-		 	if(rbxposition.transform.localPosition.x >= .18) {
-		 		tempPosition = new Vector3(.18f,.22f,0);
+		 	if(rbxposition.transform.localPosition.z >= .22) {
+		 		tempPosition = new Vector3(0f,.15f,.22f);
 		 		}
 		 	else{
-				tempPosition = new Vector3(-.18f,.22f,0);
+				tempPosition = new Vector3(0f,.15f,-.22f);
 		 	}
 		 }
 	
